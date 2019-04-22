ede30e7a73b1e83eb1b74ffb9754a8d0e2cb7c00
sserges <agounkeserge@gmail.com>
2019-04-22 10:58:47 +0000

    creating the action counter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
-import { SET_AUTHENTIFICATION } from "./action-types";
+import { SET_AUTHENTIFICATION, INCREMENT_ACTION_COUNT } from "./action-types";
 
 export function setAuthentification(isLoggedIn) {
+  return function(dispatch) {
+    dispatch({
+      type: SET_AUTHENTIFICATION,
+      payload: isLoggedIn
+    });
+    dispatch(incrementActionCount());
+  };
+}
+
+export function incrementActionCount() {
   return {
-    type: SET_AUTHENTIFICATION,
-    payload: isLoggedIn
+    type: INCREMENT_ACTION_COUNT
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import AuthentificationReducer from "./authentification";
+import ActionInfoReducer from "./action-info";
 
 const rootReducer = combineReducers({
-  authentification: AuthentificationReducer
+  authentification: AuthentificationReducer,
+  actionInfo: ActionInfoReducer
 });
 
 export default rootReducer;
