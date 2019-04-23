c9fbb5905c506f71516881592efff639294b7e83
sserges <agounkeserge@gmail.com>
2019-04-23 19:03:44 +0000

    Connection to the database

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const http = require("http");
+const mongoose = require("mongoose");
 
 const router = require("./route");
+
+mongoose.connect(
+  "mongodb+srv://robin:robin@cluster0-p34fz.mongodb.net/test?retryWrites=true"
+);
+mongoose.connection
+  .once("open", () => console.log("Connecté à MongoDB"))
+  .on("error", error => console.log("Erreur de connexion à MongoDB : ", error));
 
 const expressServer = express();
 expressServer.use(morgan("combined"));
