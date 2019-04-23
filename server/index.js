447fccc0ca880577f3f8de854f0ec406382488ef
sserges <agounkeserge@gmail.com>
2019-04-23 18:00:36 +0000

    mount the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("Hello word!");
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const http = require("http");
+
+const expressServer = express();
+
+const router = require("./route");
+
+expressServer.use(morgan("combined"));
+expressServer.use(bodyParser.json({ type: "*/" }));
+
+const port = 3090;
+const server = http.createServer(expressServer);
+server.listen(port);
+console.log("Le serveur écoute sur le port : ", port);
