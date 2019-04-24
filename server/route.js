23edfd866928095a02d5513fcbebdc374ef58043
sserges <agounkeserge@gmail.com>
2019-04-24 15:13:30 +0000

    secure request

--- server/route.js
+++ server/route.js
@@ -6,9 +6,8 @@ const passport = require("passport");
 const requireToken = passport.authenticate("jwt", { session: false });
 
 module.exports = function(expressServer) {
-  expressServer.get("/", function(req, res, next) {
-    res.send({ serverData: ["Stratocaster", "Gibson", "Ibanez"] });
-  });
-
   expressServer.post("/signup", AuthentificationController.signup);
+  expressServer.get("/ressourceSecrete", requireToken, function(req, res) {
+    res.send({ codeDeLaMort: 42 });
+  });
 };
