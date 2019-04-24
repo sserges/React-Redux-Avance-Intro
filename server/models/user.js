100ef84c157020b95dfdbea6c1e1fa6e256b96a2
sserges <agounkeserge@gmail.com>
2019-04-24 12:27:15 +0000

    Use of bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+
 const Schema = mongoose.Schema;
 
 // Création du model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+userSchema.pre("save", function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 userSchema.set("autoIndex", false);
